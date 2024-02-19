33fd0d638275c092b1102cd5b7b2516f32b6eb0f
Nicholas Omer Chiasson <nicholasomerchiasson@gmail.com>
2024-02-19 12:35:24 -0500

    Got some updating happening

--- 2024/src/main.js
+++ 2024/src/main.js
@@ -4,6 +4,12 @@ const UPDATE_DELAY = 1000 / 60; // 60 times per second
 const CMP_PLAYER = 0;
 const CMP_POSITION = 1;
 const CMP_TEXTURE = 2;
+const CMP_VELOCITY = 3;
+const CMP_DIMENSIONS = 4;
+const CMP_PLATFORM = 5;
+
+const GRAVITY = 28;
+const JUMP_FORCE = 18;
 
 // register event handlers
 addEventListener("load", dispatchInitialize);
@@ -36,6 +42,7 @@ class GameState {
   gameData;
   input;
   previousTime;
+  world;
   
   constructor(canvas, context, gameData) {
     this.canvas = canvas;
@@ -44,6 +51,7 @@ class GameState {
     this.gameData = gameData;
     this.input = new Input();
     this.previousTime = performance.now();
+    this.world = new Vector(0, 0);
     canvas.addEventListener("click", async function() {
       try {
         await canvas.requestPointerLock();
@@ -248,11 +256,20 @@ function dispatchUpdate(gameState) {
   gameState.previousTime = performance.now();
   const deltaTime = (gameState.previousTime - previousTime) * 0.001;
   update(gameState, deltaTime);
+  for (const entity of gameState.entities) {
+    if (entity.component[CMP_POSITION]) {
+      entity.component[CMP_POSITION].x += gameState.world.x;
+      entity.component[CMP_POSITION].y += gameState.world.y;
+    }
+  }
+  gameState.world.x = 0;
+  gameState.world.y = 0;
   gameState.input.update(gameState, deltaTime);
   setTimeout(dispatchUpdate, UPDATE_DELAY, gameState);
 }
 
 function dispatchDraw(gameState) {
+  gameState.context.clearRect(0, 0, gameState.canvas.width, gameState.canvas.height);
   draw(gameState);
   requestAnimationFrame(function() {
     dispatchDraw(gameState);
@@ -261,36 +278,93 @@ function dispatchDraw(gameState) {
 
 // game logic
 function initialize(gameState) {
-  const player = new Entity();
-  player.addComponent(CMP_PLAYER, true);
-  player.addComponent(CMP_POSITION, {x: 0, y: 0});
-  // player.addComponent(CMP_TEXTURE, Texture.FromUrl("/rsrc/images/birthday-cake.png"));
-  player.update = updatePlayer;
-  player.draw = drawEntity;
-  gameState.addEntity(player);
+  gameState.addEntity(newPlayer(gameState));
+  gameState.addEntity(newPlatform(gameState, new Vector(0, 0)));
 }
 
 function update(gameState, deltaTime) {
   for (const entity of gameState.entities) {
-    entity.update(gameState, deltaTime);
+    if (entity.update) {
+      entity.update(gameState, deltaTime);
+    }
   }
 }
 
 function draw(gameState) {
   for (const entity of gameState.entities) {
-    entity.draw(gameState);
+    if (entity.draw) {
+      entity.draw(gameState);
+    }
   }
 }
 
 // systems
+function newPlayer(gameState) {
+  const player = new Entity();
+  const playerWidth = gameState.canvas.width / 6;
+  player.addComponent(CMP_PLAYER, true);
+  player.addComponent(CMP_POSITION, new Vector(gameState.canvas.width / 2 - playerWidth / 2, gameState.canvas.height / 2 - playerWidth / 2));
+  player.addComponent(CMP_VELOCITY, new Vector(0, 0));
+  player.addComponent(CMP_TEXTURE, Texture.FromUrl("/rsrc/images/birthday-cake.png"));
+  player.addComponent(CMP_DIMENSIONS, new Vector(playerWidth, playerWidth));
+  player.update = updatePlayer;
+  player.draw = drawEntity;
+  return player;
+}
+
 function updatePlayer(gameState, deltaTime) {
-  // todo
+  // calculate velocity
+  const mouseMovement = gameState.input.mouse.movement;
+  if (document.pointerLockElement) {
+    this.component[CMP_VELOCITY].x = mouseMovement.x / 2;
+  }
+  // if (document.pointerLockElement && mouseMovement.x !== 0) {
+  //   this.component[CMP_VELOCITY].x = mouseMovement.x / 2;
+  // } else {
+  //   this.component[CMP_VELOCITY].x -= this.component[CMP_VELOCITY].x * deltaTime * 10;
+  // }
+
+  this.component[CMP_VELOCITY].y += GRAVITY * deltaTime;
+
+  this.component[CMP_POSITION].x += this.component[CMP_VELOCITY].x;
+  this.component[CMP_POSITION].y += this.component[CMP_VELOCITY].y;
+
+  // calculate collisions
+  const bottom = this.component[CMP_POSITION].y + this.component[CMP_DIMENSIONS].y;
+  const screenIntersection = bottom - gameState.canvas.height;
+  if (screenIntersection > 0) {
+    this.component[CMP_POSITION].y -= screenIntersection * 2;
+    this.component[CMP_VELOCITY].y = -JUMP_FORCE;
+  }
+
+  this.component[CMP_POSITION].x = (this.component[CMP_POSITION].x + gameState.canvas.width) % gameState.canvas.width;
+
+  const moveUpBoundary = gameState.canvas.height / 2 - this.component[CMP_POSITION].y - this.component[CMP_DIMENSIONS].y / 2;
+  if (moveUpBoundary > 0) {
+    gameState.world.y += moveUpBoundary;
+  }
+}
+
+function newPlatform(gameState, position) {
+  const platform = new Entity();
+  const platformWidth = gameState.canvas.width / 4;
+  platform.addComponent(CMP_PLATFORM, true);
+  platform.addComponent(CMP_POSITION, position);
+  platform.addComponent(CMP_TEXTURE, Texture.FromUrl("/rsrc/images/birthday-cake.png"));
+  platform.addComponent(CMP_DIMENSIONS, new Vector(platformWidth, platformWidth / 5));
+  platform.draw = drawEntity;
+  return platform;
 }
 
 function drawEntity(gameState) {
   const position = this.component[CMP_POSITION];
   const texture = this.component[CMP_TEXTURE];
-  if (position && texture) {
-    gameState.context.drawImage(texture, position.x, position.y);
+  const dimensions = this.component[CMP_DIMENSIONS];
+  if (position && texture && dimensions) {
+    gameState.context.drawImage(texture, position.x, position.y, dimensions.x, dimensions.y);
+    const screenIntersection = position.x + dimensions.x - gameState.canvas.width;
+    if (screenIntersection > 0) {
+      gameState.context.drawImage(texture, screenIntersection - dimensions.x, position.y, dimensions.x, dimensions.y);
+    }
   }
 }
